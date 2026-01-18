9966bc4b5878aae68a5bc991e389858dc43526d2
Vasil Kalchev <vase7u@gmail.com>
2026-01-18 17:37:17 +0200

    Update exports.

--- hw/export/export_sheets.sh
+++ hw/export/export_sheets.sh
@@ -1,4 +1,4 @@
-kicad-cli sch export pdf -o sch.pdf ../LEDDs.kicad_sch
+kicad-cli sch export pdf -n -o sch.pdf ../LEDDs.kicad_sch
 
 kicad-cli pcb export svg -o pcb-sheet.svg -l B.Cu,B.Mask,B.Silkscreen,F.Cu,F.Mask,F.Silkscreen,Edge.Cuts ../LEDDs.kicad_pcb
 kicad-cli pcb export svg -o f-pcb-sheet.svg -l F.Cu,F.Mask,F.Silkscreen,Edge.Cuts ../LEDDs.kicad_pcb
